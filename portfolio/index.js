b1e88e6a5a5bc29d1b07adb0f2978a9b0d7bab61
beenar-0 <dncastledn@gmail.com>
2022-01-25 03:04:53 +0300

    feat: add burger-menu

---
+++ portfolio/index.js
@@ -0,0 +1,54 @@
+const burgerIcon = document.querySelector(".menu-icon")
+const menuBody = document.querySelector(".menu__body")
+const link1 = document.querySelector(".link1")
+const link2 = document.querySelector(".link2")
+const link3 = document.querySelector(".link3")
+const link4 = document.querySelector(".link4")
+const link5 = document.querySelector(".link5")
+
+burgerIcon.addEventListener('click', function (a) {
+   menuBody.classList.toggle("_active")
+   burgerIcon.classList.toggle("_active")
+   document.body.classList.toggle("_lock")
+})
+
+link1.addEventListener("click", function (b) {
+   if (burgerIcon.classList.contains("_active")) {
+   menuBody.classList.remove("_active")
+   burgerIcon.classList.remove("_active")
+   document.body.classList.remove("_lock")
+}
+})
+
+link2.addEventListener("click", function (c) {
+   if (burgerIcon.classList.contains("_active")) {
+   menuBody.classList.remove("_active")
+   burgerIcon.classList.remove("_active")
+   document.body.classList.remove("_lock")
+}
+})
+
+link3.addEventListener("click", function (d) {
+   if (burgerIcon.classList.contains("_active")) {
+   menuBody.classList.remove("_active")
+   burgerIcon.classList.remove("_active")
+   document.body.classList.remove("_lock")
+}
+})
+
+link4.addEventListener("click", function (e) {
+   if (burgerIcon.classList.contains("_active")) {
+   menuBody.classList.remove("_active")
+   burgerIcon.classList.remove("_active")
+   document.body.classList.remove("_lock")
+}
+})
+
+link5.addEventListener("click", function (f) {
+   if (burgerIcon.classList.contains("_active")) {
+   menuBody.classList.remove("_active")
+   burgerIcon.classList.remove("_active")
+   document.body.classList.remove("_lock")
+}
+})
+
