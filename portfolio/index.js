db60aaa8ca02075831ed1dc64f684483c69760d4
beenar-0 <dncastledn@gmail.com>
2022-01-28 02:53:59 +0300

    feat: add cedhanging of images whenе time-of-year-tton is clicked

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,6 +1,8 @@
 const burgerIcon = document.querySelector(".menu-icon")
 const menuBody = document.querySelector(".menu__body")
 const menuLinks = document.querySelectorAll('.nav-item')
+const portfolioItems = document.querySelectorAll(".portfolio-item")
+const portfolioButtons = document.querySelectorAll(".slider-btn")
 
 burgerIcon.addEventListener('click', () => {
    menuBody.classList.toggle("_active")
@@ -17,6 +19,38 @@ menuLinks.forEach((link) => {
       }
    })
 })
+
+portfolioButtons.forEach((button, buttonIndex) => {
+   button.addEventListener('click', () => {
+      portfolioItems.forEach((img,imgIndex) => {
+         switch (buttonIndex) {
+            case 0:
+               img.src = `./assets/img/winter/${imgIndex + 1}.jpg`
+                changeButtonStyle()
+                 break
+            case 1:
+               img.src = `./assets/img/spring/${imgIndex + 1}.jpg`
+               changeButtonStyle()
+                 break
+            case 2:
+               img.src = `./assets/img/summer/${imgIndex + 1}.jpg`
+               changeButtonStyle()
+                 break
+            case 3:
+               img.src = `./assets/img/autumn/${imgIndex + 1}.jpg`
+               changeButtonStyle()
+                 break
+         }
+      })
+   })
+})
+
+function changeButtonStyle () {
+   portfolioButtons.forEach((button) => {
+      button.classList.remove("_active-button")
+      event.target.classList.add("_active-button")
+      })
+}
 
 
 
