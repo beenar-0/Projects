6fe6d36f7f78e54cda7d4f3e803011d882b355e6
beenar-0 <dncastledn@gmail.com>
2022-01-30 03:59:28 +0300

    refactor: refactored index.js

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,98 +1,6 @@
-// Burger-menu
+// Switching theme
 
 const switchThemeIcon = document.querySelector(".switch-theme")
-const burgerIcon = document.querySelector(".menu-icon")
-const menuBody = document.querySelector(".menu__body")
-const menuLinks = document.querySelectorAll('.nav-item')
-
-
-burgerIcon.addEventListener('click', () => {
-    menuBody.classList.toggle("_active")
-    burgerIcon.classList.toggle("_active")
-    document.body.classList.toggle("_lock")
-    switchThemeIcon.classList.toggle("_hide")
-})
-
-menuLinks.forEach((link) => {
-    link.addEventListener('click', () => {
-        if (burgerIcon.classList.contains('_active')) {
-            menuBody.classList.remove("_active")
-            burgerIcon.classList.remove("_active")
-            document.body.classList.remove("_lock")
-        }
-    })
-})
-
-
-// Changing portfolio images
-
-const portfolioItems = document.querySelectorAll(".portfolio-item")
-const portfolioButtons = document.querySelectorAll(".slider-btn")
-
-portfolioButtons.forEach((button, buttonIndex) => {
-    button.addEventListener('click', () => {
-        portfolioItems.forEach((img, imgIndex) => {
-            switch (buttonIndex) {
-                case 0:
-                    img.src = `./assets/img/winter/${imgIndex + 1}.jpg`
-                    changeButtonStyle()
-                    break
-                case 1:
-                    img.src = `./assets/img/spring/${imgIndex + 1}.jpg`
-                    changeButtonStyle()
-                    break
-                case 2:
-                    img.src = `./assets/img/summer/${imgIndex + 1}.jpg`
-                    changeButtonStyle()
-                    break
-                case 3:
-                    img.src = `./assets/img/autumn/${imgIndex + 1}.jpg`
-                    changeButtonStyle()
-                    break
-            }
-        })
-    })
-})
-
-function changeButtonStyle() {
-    portfolioButtons.forEach((button) => {
-        button.classList.remove("_active-button")
-        event.target.classList.add("_active-button")
-    })
-}
-
-
-// Preload images
-
-const timesOfYear = ['winter', 'spring', 'summer', 'autumn']
-
-function preloadImages() {
-    timesOfYear.forEach((timeOfYear) => {
-        for (let i = 0; i < 6; i++) {
-            const img = new Image()
-            img.src = `./assets/img/${timeOfYear}/${i + 1}.jpg`
-        }
-    })
-
-}
-
-preloadImages()
-
-
-// "Send" button
-
-const sendButton = document.querySelector(".send-button")
-const sent = document.querySelector(".sent")
-
-sendButton.addEventListener('click', () => {
-    sent.classList.add('_active')
-    setTimeout(() => {
-        sent.classList.remove("_active")
-    }, 3000)
-})
-
-
-// Switching theme
 
 switchThemeIcon.addEventListener('click', () => {
     switchThemeIcon.classList.toggle("_light")
@@ -182,4 +90,98 @@ function changeTheme() {
         document.documentElement.style.setProperty('--theme-icon-hover', 'invert(14%) sepia(10%) saturate(1028%) hue-rotate(7deg) brightness(84%) contrast(77%)')
     }
 }
+
+
+// Changing portfolio images
+
+const portfolioItems = document.querySelectorAll(".portfolio-item")
+const portfolioButtons = document.querySelectorAll(".slider-btn")
+
+portfolioButtons.forEach((button, buttonIndex) => {
+    button.addEventListener('click', () => {
+        portfolioItems.forEach((img, imgIndex) => {
+            switch (buttonIndex) {
+                case 0:
+                    img.src = `./assets/img/winter/${imgIndex + 1}.jpg`
+                    changeButtonStyle()
+                    break
+                case 1:
+                    img.src = `./assets/img/spring/${imgIndex + 1}.jpg`
+                    changeButtonStyle()
+                    break
+                case 2:
+                    img.src = `./assets/img/summer/${imgIndex + 1}.jpg`
+                    changeButtonStyle()
+                    break
+                case 3:
+                    img.src = `./assets/img/autumn/${imgIndex + 1}.jpg`
+                    changeButtonStyle()
+                    break
+            }
+        })
+    })
+})
+
+function changeButtonStyle() {
+    portfolioButtons.forEach((button) => {
+        button.classList.remove("_active-button")
+        event.target.classList.add("_active-button")
+    })
+}
+
+
+// Preload images
+
+const timesOfYear = ['winter', 'spring', 'summer', 'autumn']
+
+function preloadImages() {
+    timesOfYear.forEach((timeOfYear) => {
+        for (let i = 0; i < 6; i++) {
+            const img = new Image()
+            img.src = `./assets/img/${timeOfYear}/${i + 1}.jpg`
+        }
+    })
+
+}
+
+preloadImages()
+
+
+// "Send" button
+
+const sendButton = document.querySelector(".send-button")
+const sent = document.querySelector(".sent")
+
+sendButton.addEventListener('click', () => {
+    sent.classList.add('_active')
+    setTimeout(() => {
+        sent.classList.remove("_active")
+    }, 3000)
+})
+
+
+// Burger-menu
+
+const burgerIcon = document.querySelector(".menu-icon")
+const menuBody = document.querySelector(".menu__body")
+const menuLinks = document.querySelectorAll('.nav-item')
+
+
+burgerIcon.addEventListener('click', () => {
+    menuBody.classList.toggle("_active")
+    burgerIcon.classList.toggle("_active")
+    document.body.classList.toggle("_lock")
+    switchThemeIcon.classList.toggle("_hide")
+})
+
+menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        if (burgerIcon.classList.contains('_active')) {
+            menuBody.classList.remove("_active")
+            burgerIcon.classList.remove("_active")
+            document.body.classList.remove("_lock")
+        }
+    })
+})
+
 
