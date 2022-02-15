2641067fea8cf7b3c3c147aef40d653abd064eca
beenar-0 <dncastledn@gmail.com>
2022-02-15 04:37:26 +0300

    feat: added search at index.js and search-button

--- movie-app/index.js
+++ movie-app/index.js
@@ -1,23 +1,44 @@
-const container = document.querySelector('.movies__container')
+// When page load
+async function getStartData() {
+    const res = await fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=aaa9fb0e895ccbbb2c96e6632244adfd`);
+    const data = await res.json();
+    const results = data['results']
+    results.forEach((item) => {
+        buildNewCard(item['original_title'], item['poster_path'], item['overview'], item['vote_average'])
+    })
+}
+
+getStartData()
+
+
+// search
+const input = document.querySelector('.search')
+
+function search() {
+    document.querySelector('.movies__container').innerHTML = ''
+    getData(input.value)
+}
+
+input.addEventListener('change', search)
+
+const searchButton = document.querySelector('.search-icon')
+searchButton.addEventListener('click', search)
 
 async function getData(req) {
     const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=aaa9fb0e895ccbbb2c96e6632244adfd&query=${req}`);
     const data = await res.json();
     const results = data['results']
-    let count = 0
     results.forEach((item) => {
         buildNewCard(item['original_title'], item['poster_path'], item['overview'], item['vote_average'])
     })
-    console.log(results);
 }
 
-getData('action');
+
+// build movie-card function
+const container = document.querySelector('.movies__container')
+
 
 function buildNewCard(title, cover, overview, score) {
-    // console.log(title)
-    // console.log(cover)
-    // console.log(overview)
-    // console.log(score)
     const newCard = document.createElement('div')
     newCard.classList.add('movies-item')
     container.appendChild(newCard)
@@ -27,7 +48,11 @@ function buildNewCard(title, cover, overview, score) {
     newCard.appendChild(newOverview)
     const newCover = document.createElement('img')
     newCover.classList.add('movie-cover')
-     newCover.src = 'https://image.tmdb.org/t/p/w500'+cover
+    if (cover === null) {
+        newCover.src = 'http://lexingtonvenue.com/media/poster-placeholder.jpg'
+    } else {
+        newCover.src = 'https://image.tmdb.org/t/p/w500' + cover
+    }
     newCover.alt = `image not found (${title})`
     newCard.appendChild(newCover)
     const newInfo = document.createElement('div')
@@ -43,8 +68,11 @@ function buildNewCard(title, cover, overview, score) {
     newInfo.appendChild(newScore)
 }
 
+//self rate
+console.log('Результат самопроверки:70/60')
+console.log('Дополнительный функционал не входящий в основные пункты ТЗ: добавлены рейтинг и описание фильмов.')
+console.log(' Оформление так же отличается от демо ( надеюсь, что в лучшую сторону :) )')
 
-// data['results']   results[{movieNumber}]['original_title']   title
-// data['results']   results[{movieNumber}]['backdrop_path']    cover
-// data['results']   results[{movieNumber}]['overview']         overview
-// data['results']   results[{movieNumber}]['vote_average']     score
+
+
+
